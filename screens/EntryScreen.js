c0cb49c54550ab201ba767c9608ad6375171dcc2
Willem <willemtoews@gmail.com>
2025-12-28 22:27:36 -0700

    Remove RECEIVE_SMS permission and related SMS listener logic; update EntryScreen for SMS handling

--- screens/EntryScreen.js
+++ screens/EntryScreen.js
@@ -1,9 +1,11 @@
 import { View, PermissionsAndroid, Pressable, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useEffect } from 'react';
-import { requestReadSMSPermission, requestReceiveSMSPermission}from '../utils/permissions';
+import { requestReadSMSPermission}from '../utils/permissions';
 import { getSecure } from '../utils/overrides';
 import { styles } from '../StyleSheet';
+
+import SmsListener from 'react-native-android-sms-listener'
 
 export default function EntryScreen() {
   const navigation = useNavigation();
@@ -38,18 +40,34 @@ export default function EntryScreen() {
     } else {
       console.log("SMS permission granted");
     }
-    const receiveGranted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.RECEIVE_SMS);
-    if (receiveGranted !== PermissionsAndroid.RESULTS.GRANTED) {
-      requestReceiveSMSPermission();
-      console.log("Receive SMS permission not granted");
-    } else {
-      console.log("Receive SMS permission granted");
-    }
-  }; 
+  }
+
+  const testSMSListener = () => {
+    console.log("Testing SMS Listener...");
+    SmsListener.addListener(message => {
+      console.log('RAW SMS RECEIVED:');
+      console.log('From:', message.originatingAddress);
+      console.log('Body:', message.body);
+      console.log('Timestamp:', message.timestamp);
+
+      //clean up listener after first message
+      SmsListener.removeListener();
+      console.log('SMS listener removed');
+    });
+
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.EntryScreen.LoginButtonContainer}>
+         <Pressable
+          onPress={testSMSListener}
+          style={({ pressed }) => [
+            styles.EntryScreen.loginButton,
+            pressed && styles.EntryScreen.loginButton.pressed
+          ]}>
+          <Text style={styles.EntryScreen.loginButtonText}>Test SMS Listener</Text>
+        </Pressable> 
         <Pressable
           onPress={() => navigation.replace('Home')}
           style={({ pressed }) => [
